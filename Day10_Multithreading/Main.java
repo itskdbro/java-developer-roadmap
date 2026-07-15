760c4c2507a8cfa6ccdedfbca05fba4ec133e348
itskdbro <keshavdhawan2002@gmail.com>
2026-07-15 17:23:54 +0530

    Day10&11 - Multithreading

--- Day10_Multithreading/Main.java
+++ Day10_Multithreading/Main.java
@@ -2,12 +2,13 @@ package Day10_Multithreading;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 //        System.out.println(Thread.currentThread().getName()); // - Main Thread created by JVM
 
         // Thread Class
         MyThread t1 = new MyThread();
         t1.start();
+//        t1.join();
 
         // Runnable interface
         MyThread2 t2 = new MyThread2();
